4c8a2a846986d17d09921704d5dfe23ba0230481
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-11-26 16:48:35 +0100

    aggiunta funzioni in corso

--- start.js
+++ start.js
@@ -3,28 +3,92 @@
 
 const argv   = require('yargs').argv;
 const CwLogs = require('./cwlogs');
+const path   = require('path');
 
-const logGroupName = argv._[0];
-const region       = argv._[1];
+let config;
+let configPath = path.join(__dirname, 'config.json');
+try {
+  config = require(configPath);
+} catch(e) {
+  config = {};
+}
 
-if (!logGroupName || !region) return console.log(`
-missing params.
+const commands = {
+  list: () => {
+    const inquirer = require('inquirer');
 
-cwlogs [logGroupName] [region] [options]
+    const prompt = inquirer.createPromptModule();
 
---timeformat\t-t\t\tmomentjs time format;
---interval\t-i\t\tinterval between every log request;
---logformat\t-f\t\twhile watching logs generated from lambda functions the output is more readable if this options is set to "lambda";
---streamname\t-n\t\tif not specified logs will be printed from the last stream name found;
-`);
+    prompt([
+      {type: 'list', name: 'name', message: 'message', choices: [
+        'a', 'b', 'c'
+      ]}
+    ]).then(/* ... */);
+  },
 
-const cwlogs = new CwLogs({
-  logGroupName: logGroupName,
-  region: region,
-  logStreamName: argv.streamname || argv.n,
-  momentTimeFormat: argv.timeformat || argv.t || 'hh:mm:ss:SSS',
-  interval: argv.interval || argv.i || 2000,
-  logFormat: argv.logformat || argv.f || 'standard'
-});
+  help: () => {
+    console.log([
+      'cwlogs [logGroupName] [region] [options]',
+      '--timeformat\t-t\t\tmomentjs time format;',
+      '--interval\t-i\t\tinterval between every log request;',
+      '--logformat\t-f\t\twhile watching logs generated from lambda functions the output is more readable if this options is set to "lambda";',
+      '--streamname\t-n\t\tif not specified logs will be printed from the last stream name found;'
+    ].join('\n'));
+  },
 
-cwlogs.start();
+  add: (options) => {
+    options = options || {};
+    const macroName = options.macroName || argv._[1];
+
+    config[macroName] = {
+      logGroupName: options.logGroupName || argv._[2],
+      region: options.region || argv._[3],
+      logStreamName: options.streamname || argv.streamname || argv.n,
+      momentTimeFormat: options.timeformat || argv.timeformat || argv.t,
+      interval: options.interval || argv.interval || argv.i,
+      logFormat: options.logformat || argv.logformat || argv.f
+    };
+
+    try {
+      require('fs').writeFileSync(configPath, JSON.stringify(config, null, 2));
+      console.log(`Successfully added ${macroName} to config.json`)
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  remove: (macroName) => {
+
+  },
+
+  startLogging: (options) => {
+    options = options || {};
+
+    const logGroupName =  options.logGroupName || argv._[0];
+    const region = options.region || argv._[1];
+
+    if (!logGroupName || !region) {
+      console.log('missing params\n');
+      return commands.help();
+    }
+
+    const cwlogs = new CwLogs({
+      logGroupName: logGroupName,
+      region: region,
+      logStreamName: options.streamname || argv.streamname || argv.n,
+      momentTimeFormat: options.timeformat || argv.timeformat || argv.t || 'hh:mm:ss:SSS',
+      interval: options.interval || argv.interval || argv.i || 2000,
+      logFormat: options.logformat || argv.logformat || argv.f || 'standard'
+    });
+
+    cwlogs.start();
+  }
+};
+
+
+if (!argv._[0]) return commands.list();
+
+const command = commands[argv._[0]];
+if (command) return command();
+
+commands.startLogging();
