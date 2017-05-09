722825a8b1d5e4303c0e0fd91f8e8bfc1ab93873
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-05-09 15:08:54 +0200

    test

--- cwlogs.js
+++ cwlogs.js
@@ -5,23 +5,22 @@ const clc    = require('cli-color');
 const moment = require('moment');
 const path   = require('path');
 
-const _options = {
-  logGroupName: '',
-  region: '',
-  momentTimeFormat: 'hh:mm:ss:SSS',
-  interval: 2000,
-  logFormat: 'standard'
-};
-
 module.exports = class CwLogs {
   constructor(options) {
-    this.options = Object.assign({}, _options, options);
+    this.options = Object.assign({
+      logGroupName: '',
+      region: '',
+      momentTimeFormat: 'hh:mm:ss:SSS',
+      interval: 2000,
+      logFormat: 'standard'
+    }, options);
+
     this.lastLogTime = 0;
     return this;
-  };
+  }
 
   start(){
-    const cloudwatchlogs = new AWS.CloudWatchLogs( {region: this.options.region} );
+    const cloudwatchlogs = new AWS.CloudWatchLogs({ region: this.options.region });
     this.interval = setInterval(() => {
       const params = {
         logGroupName: this.options.logGroupName,
@@ -50,16 +49,17 @@ module.exports = class CwLogs {
           if (!events.length) return;
 
           let logFunction = (message, timestamp) => console.log(timestamp, message);
-          if (this.options.logFormat && this.options.logFormat !== 'default') {
+          const { logFormat } = this.options;
+          if (logFormat && logFormat !== 'default') {
 
-            if (typeof(logFunction) == "function") {
-              logFunction = this.options.logFormat;
-            } else {
+            if (typeof(logFormat) === 'function') {
+              logFunction = logFormat;
+            }
+            else {
               try {
                 logFunction = require(path.join(__dirname, 'log-formats', `${this.options.logFormat}.js`));
               } catch(err) {}
             }
-            
           }
 
           this.lastLogTime = events[events.length - 1].timestamp + 1;
@@ -70,7 +70,6 @@ module.exports = class CwLogs {
             const timestamp = `[ ${clc.blackBright(moment(event.timestamp).format(this.options.momentTimeFormat))} ]`;
             logFunction(message, timestamp, event);
           }
-
         });
       });
     }, this.options.interval);
